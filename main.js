0c138029e4e4ec63d0eaebdf4844e381d30d266a
Zistaeb <bperezatencio@gmail.com>
2023-07-14 15:26:36 -0300

    clase 23

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const menuHamburguerIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
@@ -15,10 +15,10 @@ function toggleDesktopMenu() {
 };
 
 function toggleMobileMenu() {
-    const isAsideClose = aside.classList.contains("inactive");
+    const isAsideClose = shoppingCartContainer.classList.contains("inactive");
 
     if(!isAsideClose) {
-        aside.classList.add("inactive"); 
+        shoppingCartContainer.classList.add("inactive"); 
     }
 
     mobileMenu.classList.toggle("inactive");
@@ -31,7 +31,7 @@ function toggleCarritoAside() {
         mobileMenu.classList.add("inactive"); 
     }
 
-    aside.classList.toggle("inactive");
+    shoppingCartContainer.classList.toggle("inactive");
 
 }
 
